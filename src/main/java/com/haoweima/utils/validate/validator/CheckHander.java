b1d48a21aefaa0b6644a1ce2ffe95a34d8e63d85
haoweima <haoweima@creditease>
2019-01-23 14:40:42 +0800

    1.0.1 校验器

--- src/main/java/com/haoweima/utils/validate/validator/CheckHander.java
+++ src/main/java/com/haoweima/utils/validate/validator/CheckHander.java
@@ -1,6 +1,8 @@
 package com.haoweima.utils.validate.validator;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * 传送带
@@ -40,5 +42,39 @@ public class CheckHander {
             }
         }
         return result;
+    }
+
+    private static class Inspector<T extends Validator> {
+
+        private T[] checkRules;
+        private Object object;
+
+        Inspector(T[] checkRules, Object object) {
+            this.checkRules = checkRules;
+            this.object = object;
+        }
+
+        CheckResult docheck() throws Exception{
+            Class clz = object.getClass();
+            Field[] fields = clz.getDeclaredFields();
+            CheckResult result = new CheckResult();
+
+            if(checkRules.length != fields.length){
+                result = new CheckResult();
+                result.setResultCode(ResultEnum.FAIL);
+                result.setResultMsg("检查条例和实体类数量不匹配！");
+                return result;
+            }
+
+            for(int i = 0; i < checkRules.length; i++){
+                String fieldName = fields[i].getName();
+                Method method = clz.getDeclaredMethod("get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1));
+                result = checkRules[i].doValidate(method.invoke(object));
+
+                if(result.resultCode == ResultEnum.FAIL) break;
+            }
+
+            return result;
+        }
     }
 }
